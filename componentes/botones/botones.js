7aa9bf436304eb0af77ae18c47cb164dd9336297
JordanBarrera-dev <jbarcar0319@gmail.com>
2026-04-24 20:22:56 -0500

    changes

--- componentes/botones/botones.js
+++ componentes/botones/botones.js
@@ -5,8 +5,4 @@ return `
 </div>
 `
 }
-
-//<div class="logo-area">
-//<h5 class="nav-title" style="margin:0; font-weight:bold;">${title}</h5>
-//<p class="nav-text" style="margin:0; font-size:0.8rem;">${description}</p>
-//</div>
+//botones - lo mejor es modificar la clase o contenedor y asi les modificamos las propiedades.
