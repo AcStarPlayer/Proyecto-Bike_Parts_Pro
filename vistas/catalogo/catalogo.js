3b25d5c288d5401867037e6a9d87d065f3f7f132
Leisy Sánchez <leisysado@gmail.com>
2026-04-27 18:02:14 -0500

    arreglo de responsive

--- vistas/catalogo/catalogo.js
+++ vistas/catalogo/catalogo.js
@@ -40,7 +40,7 @@ productos.forEach((producto) => {
   `;
 
   html += `
-    <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
       ${tarjetasConImagen(
         producto.titulo,
         `${producto.precio.toLocaleString("es-CO", {
