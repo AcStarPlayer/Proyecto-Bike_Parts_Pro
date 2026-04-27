e6ae496358391d2f4dd5c10915d5b7b57cb6e291
feralayon <feralayon@gmail.com>
2026-04-27 03:31:13 -0500

    corrije type

--- componentes/botones/botones.js
+++ componentes/botones/botones.js
@@ -1,7 +1,7 @@
 
-export function botones(title, extraClass = "primary"){
-return `
-<button type="button" class="btn btn-${extraClass} w-100 btn-action">${title}</button>
-`
+export function botones(title, extraClass = "primary", type = "button") {
+  return `
+    <button type="${type}" class="btn btn-${extraClass} w-100 btn-action">${title}</button>
+  `;
 }
 //botones - lo mejor es modificar la clase o contenedor y asi les modificamos las propiedades.
