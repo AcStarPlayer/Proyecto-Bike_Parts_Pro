1d368e6a4d932d34e07c0f2f687d075902245bff
JordanBarrera-dev <jbarcar0319@gmail.com>
2026-05-02 21:02:41 -0500

    changes

--- componentes/barraNavegacion/barNav.js
+++ componentes/barraNavegacion/barNav.js
@@ -29,6 +29,10 @@ export function navBar(description, basePath = "") {
       <input type="text" placeholder="Busca tu repuesto aquí">
       <button>Buscar</button>
     </div>
+    <div><button class="ingreso"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+</svg><button></div>
     <button class="menu-toggle p-2 rounded-1" id="hamburguesa">
       <span class="bar"></span>
       <span class="bar"></span>
@@ -46,4 +50,7 @@ export function navBar(description, basePath = "") {
   botonMenu.addEventListener("click", () => {
     listaLink.classList.toggle("active");
   });
+  //display del navbar
+  const partes = ["sillin", "silla", "timon","Freno regular","Freno de disco","llanta","rueda","casco","corazas","marco","cuadro"]
+
 }
