f63c0eeb31b71ea6366b1a37ea1804740ad7a682
Leisy Sánchez <leisysado@gmail.com>
2026-05-26 16:21:25 -0500

    fix styles in navBar

--- componentes/barraNavegacion/barNav.js
+++ componentes/barraNavegacion/barNav.js
@@ -1,3 +1,5 @@
+import { partes } from "../../apis/producto-busqueda.js";
+
 export function navBar(description, basePath = "") {
   const container = document.createElement("nav");
   container.className = "nav-content sticky-top";
@@ -26,9 +28,13 @@ export function navBar(description, basePath = "") {
       </div>
     </div>
       <div class="search-container">
-      <input id=busqueda type="text" placeholder="Busca tu repuesto aquí">
+      <div class="busqueda-container">
+        <input id=busqueda type="text" placeholder="Busca tu repuesto aquí">
+        <button><i class="bi bi-search"></i></button>
+      </div>
+      
       <div id=desplegado></div>
-      <button>Buscar</button>
+      
     </div>
 </svg><button></div>
     <button class="menu-toggle p-2 rounded-1" id="hamburguesa">
@@ -41,10 +47,7 @@ export function navBar(description, basePath = "") {
     <ul class="botones-nav p-0" id="nav-list">
       ${viewsHtml}
     </ul>
-    <div><button class="ingreso"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-</svg><button></div>
+    <button class="ingreso rounded-1"><i class="bi bi-person"></i><span class="ingreso-text">Ingresa</span></button>
   `;
 
   const botonMenu = container.querySelector("#hamburguesa");
@@ -53,9 +56,7 @@ export function navBar(description, basePath = "") {
   botonMenu.addEventListener("click", () => {
     listaLink.classList.toggle("active");
   });
-  //display del navbar
-  const partes = ["sillin", "silla", "timon","Freno regular","Freno de disco","llanta","rueda","casco","corazas","marco","cuadro"]
-
+  
    const busqueda = document.getElementById("busqueda")
    const desplegado = document.getElementById("desplegado")
 
